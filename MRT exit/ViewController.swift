6ff253805c956172d18519341c23b9794bf6bfb7
Tsaorick <tsaorick@TsaorickdeMacBook-Pro.local>
2016-10-11 21:22:24 +0800

    Initial Commit

---
+++ MRT exit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRT exit
+//
+//  Created by Tsaorick on 2016/10/11.
+//  Copyright © 2016年 Tsaorick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
